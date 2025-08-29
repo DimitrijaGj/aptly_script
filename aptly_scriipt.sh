25115974d4d418949ba38638e6bc04a880ad12dd
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro-2.local>
2025-08-29 18:17:58 +0200

    aptly script added

---
+++ aptly_scriipt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+##List of Distros
+distros=("debian" "trixie")
+packages=("main" "contrib" "non-free" "non-free-firmware")
+# loop through distros
+for distro in "${distros[@]}"; do 
+    for package in "${packages[@]}"; do
+        echo "$distro and $package"
+    done
+done
